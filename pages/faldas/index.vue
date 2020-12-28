8ff0266662382320d7c57e76e250260495f84a29
Eri Vera <59424621+eriverec@users.noreply.github.com>
2020-12-28 08:28:46 -0500

    vue

--- pages/faldas/index.vue
+++ pages/faldas/index.vue
@@ -1,92 +1,73 @@
 <template>
   <div>
-    <section class="hero">
-      <div class="hero-body">
-        <div class="container">
-          <div v-for="falda in faldas.slice(0, 2)" v-bind:key="falda.slug">
-            <div class="columns">
-              <div class="column is-8 is-offset-2">
-                <figure class="image">
+    <section class="section">
+      <h1 class="title column is-8 is-offset-2">Últimas publicaciones</h1>
+      <div class="columns is-multiline ">
+        <div
+          class="column"
+          v-for="falda in faldas.slice(0, 3)"
+          v-bind:key="falda.slug"
+        >
+          <div class="box">
+            <article class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
                   <datocms-image :data="falda.imagen.responsiveImage" />
                 </figure>
               </div>
-            </div>
-
-            <section class="section">
-              <div class="columns">
-                <div class="column is-8 is-offset-2">
-                  <div class="content is-medium">
-                    <h2 class="subtitle is-4">
-                      {{ formatDate(falda.publicationDate) }}
-                    </h2>
-                    <h1 class="title">
-                      <nuxt-link :to="`/faldas/${falda.slug}`">{{
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong
+                      ><nuxt-link :to="`/faldas/${falda.slug}`">{{
                         falda.titulo
-                      }}</nuxt-link>
-                    </h1>
-                    
-                  </div>
+                      }}</nuxt-link></strong
+                    >
+                    <small
+                      ><div class="tags has-addons">
+                        <span class="tag"> <b>Categoria</b></span>
+                        <span class="tag is-primary">{{
+                          falda.categ.titulo
+                        }}</span>
+                      </div></small
+                    >
+                    <small> {{ formatDate(falda.publicationDate) }}</small>
+                    <br />
+                  </p>
                 </div>
+                <nav class="level is-mobile">
+                  <div class="level-left">
+                    <a class="level-item" aria-label="reply">
+                      <span class="icon is-small">
+                        <i class="fas fa-reply" aria-hidden="true"></i>
+                      </span>
+                    </a>
+                    <a class="level-item" aria-label="retweet">
+                      <span class="icon is-small">
+                        <i class="fas fa-retweet" aria-hidden="true"></i>
+                      </span>
+                    </a>
+                    <a class="level-item" aria-label="like">
+                      <span class="icon is-small">
+                        <i class="fas fa-heart" aria-hidden="true"></i>
+                      </span>
+                    </a>
+                  </div>
+                </nav>
               </div>
-            </section>
-
-            <div class="is-divider" />
+            </article>
           </div>
         </div>
       </div>
     </section>
-
-    
-    <!-- Articles -->
-
-    <section class="hero ">
-      <div class="hero-body">
-        <div class="container">
-          <div
-            v-for="group in Math.ceil((faldas.length - 2) / 2)"
-            v-bind:key="group"
-          >
-            <section class="section">
-              <div class="columns is-variable is-8">
-                <div
-                  v-for="(falda, index) in faldas.slice(
-                    group * 2,
-                    group * 2 + 2
-                  )"
-                  v-bind:key="falda.slug"
-                  :class="['column is-5', index === 0 && 'is-offset-1']"
-                >
-                  <div class="content is-medium">
-                    <h2 class="subtitle is-5 has-text-grey">
-                      {{ formatDate(falda.publicationDate) }}
-                    </h2>
-                    <h1 class="title has-text-black is-3">
-                      <nuxt-link :to="`/faldas/${falda.slug}`">{{
-                        falda.titulo
-                      }}</nuxt-link>
-                    </h1>
-                  </div>
-                </div>
-              </div>
-            </section>
-
-            <div class="is-divider" />
-          </div>
-        </div>
-      </div>
-    </section>
-
-    
-
-
   </div>
 </template>
 
 <script>
-import { request, gql, imageFields, seoMetaTagsFields } from '~/lib/datocms'
-import { toHead } from 'vue-datocms'
-import format from 'date-fns/format'
-import parseISO from 'date-fns/parseISO'
+import { request, gql, imageFields, seoMetaTagsFields } from "~/lib/datocms";
+import { toHead } from "vue-datocms";
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
 
 export default {
   async asyncData({ params }) {
@@ -104,7 +85,7 @@ export default {
             titulo
             slug
             publicationDate: _firstPublishedAt
-            
+
             imagen {
               responsiveImage(imgixParams: { fit: crop, ar: "16:9", w: 860 }) {
                 ...imageFields
@@ -119,21 +100,21 @@ export default {
         ${imageFields}
         ${seoMetaTagsFields}
       `
-    })
+    });
 
-    return { ready: !!data, ...data }
+    return { ready: !!data, ...data };
   },
   methods: {
     formatDate(date) {
-      return format(parseISO(date), 'PPP')
+      return format(parseISO(date), "PPP");
     }
   },
   head() {
     if (!this.ready) {
-      return
+      return;
     }
 
-    return toHead(this.site.favicon)
+    return toHead(this.site.favicon);
   }
-}
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -17,70 +17,10 @@
       </div>
     </section>
 
-    <section class="section">
-      <h1 class="title column is-8 is-offset-2">Últimas publicaciones</h1>
-      <div class="columns is-multiline ">
-        <div
-          class="column"
-          v-for="falda in faldas.slice(0, 3)"
-          v-bind:key="falda.slug"
-        >
-          <div class="box">
-            <article class="media">
-              <div class="media-left">
-                <figure class="image is-128x128">
-                  <datocms-image :data="falda.imagen.responsiveImage" />
-                </figure>
-              </div>
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong
-                      ><nuxt-link :to="`/faldas/${falda.slug}`">{{
-                        falda.titulo
-                      }}</nuxt-link></strong
-                    >
-                    <small
-                      ><div class="tags has-addons">
-                        <span class="tag"> <b>Categoria</b></span>
-                        <span class="tag is-primary">{{
-                          falda.categ.titulo
-                        }}</span>
-                      </div></small
-                    >
-                    <small> {{ formatDate(falda.publicationDate) }}</small>
-                    <br />
-                  </p>
-                </div>
-                <nav class="level is-mobile">
-                  <div class="level-left">
-                    <a class="level-item" aria-label="reply">
-                      <span class="icon is-small">
-                        <i class="fas fa-reply" aria-hidden="true"></i>
-                      </span>
-                    </a>
-                    <a class="level-item" aria-label="retweet">
-                      <span class="icon is-small">
-                        <i class="fas fa-retweet" aria-hidden="true"></i>
-                      </span>
-                    </a>
-                    <a class="level-item" aria-label="like">
-                      <span class="icon is-small">
-                        <i class="fas fa-heart" aria-hidden="true"></i>
-                      </span>
-                    </a>
-                  </div>
-                </nav>
-              </div>
-            </article>
-          </div>
-        </div>
-      </div>
-    </section>
-
     <section class="hero">
       <div class="hero-body">
         <div class="container">
+          <h1 class="title column is-8 is-offset-2">Últimas publicaciones</h1>
           <div v-for="falda in faldas.slice(0, 2)" v-bind:key="falda.slug">
             <div class="columns">
               <div class="column is-8 is-offset-2">
@@ -103,7 +43,7 @@
 
             <div class="columns">
               <div class="column is-8 is-offset-2">
-                <figure class="image" style="width: 100%;">
+                <figure class="image">
                   <datocms-image :data="falda.imagen.responsiveImage" />
                 </figure>
               </div>
@@ -116,7 +56,7 @@
     </section>
 
     <section class="section">
-      <div class="notification is-primary is-light">
+      <div class="notification is-primary is-light column is-8 is-offset-2">
         <button class="delete"></button>
         Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem
         ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis
@@ -125,7 +65,30 @@
         <a>felis venenatis</a> efficitur. Sit amet, consectetur adipiscing elit
       </div>
     </section>
-
+    <section class="section">
+      <h1 class="title column is-8 is-offset-2">Categorias destacadas</h1>  
+      <div class="style_wrapper__1BeS5 column is-8 is-offset-2">
+        <a href="https://dashboard.datocms.com/signup" class="style_root__3iCRH"
+          ><div class="style_rootInner__32CX1">
+            <div>
+              <div class="style_title__1jQC3">Start using DatoCMS today</div>
+              <div class="style_subtitle__UtQrM">
+                According to Gartner 89% of companies plan to compete primarily
+                on the basis of customer experience this year. Don't get caught
+                unprepared.
+              </div>
+            </div>
+            <div class="style_action__32sxL">
+              <div class="style_root__2jAzX">
+                <div class="style_root__-6jDT style_fsBig__vL5g8">
+                  Try it for free!
+                </div>
+              </div>
+            </div>
+          </div></a
+        >
+      </div>
+    </section>
     <!-- newsletter -->
     <section class="section">
       <div class="columns">
@@ -183,8 +146,19 @@ export default {
             publicationDate: _firstPublishedAt
 
             imagen {
-              responsiveImage(imgixParams: { fit: crop, ar: "16:9", w: 860 }) {
+              responsiveImage(
+                imgixParams: { fit: crop, ar: "16:9", w: 860, auto: format }
+              ) {
                 ...imageFields
+                srcSet
+                webpSrcSet
+                sizes
+                src
+                width
+                aspectRatio
+                alt
+                title
+                base64
               }
             }
             categ {
@@ -221,6 +195,48 @@ export default {
 }
 
 .image {
+  /*text-align: center;*/
+}
+
+.style_wrapper__1BeS5 {
+  max-width: var(--page-width-with-margin);
+  
+  padding-left: var(--page-margin);
+  padding-right: var(--page-margin);
+}
+
+.style_root__3iCRH {
+  display: block;
+  padding: calc(22.4px + 4.8vw);
+  background: #fff;
+  border-radius: 5px;
+  box-shadow: var(--box-shadow);
+  box-shadow: 0 2px 40px 0 rgba(23, 41, 64, 0.05),
+    0 5px 10px 0 rgba(23, 41, 64, 0.1);
+
+  text-decoration: none;
+  color: var(--base-body-color);
+  -webkit-transition: background 0.2s ease-in-out;
+  transition: background 0.2s ease-in-out;
+  background: linear-gradient(-30deg, #faedff 30%, #e9fcff 45%, #fff 60%);
+  background-position: 0 0;
+  background-size: 200vw 100%;
+}
+
+.style_rootInner__32CX1 {
+  max-width: 750px;
+  margin: 0 auto;
   text-align: center;
+}
+
+.style_title__1jQC3 {
+  font-family: var(--font-headline);
+  font-size: calc(19.4px + 2.55vw);
+  margin-bottom: 20px;
+  line-height: 1.1;
+}
+
+.style_root__3iCRH:hover {
+    background-position: 50%;
 }
 </style>
